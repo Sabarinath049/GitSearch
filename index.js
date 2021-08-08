8cea12542d00426a5981e3de81398aa9972270a5
Sabarinath049 <tejachillapalli@gmail.com>
2021-08-08 17:08:42 +0530

    implemented display user function

--- index.js
+++ index.js
@@ -18,19 +18,55 @@ submitButtonElement.onclick=function(){
 }
 
 clearButtonElement.onclick=function(){
-    inputElement.value=""
+    if (inputElement.value!==""){
+        inputElement.value=""
+    }
+    else{
+        window.alert("Already Cleared")
+    }
 }
 
-function getData(){
-    const url = `https://api.github.com/users/${usernameValue}?client_id=${client_id}&client_secret=${client_secret}`
+function displayUserProfile(eachItem){
+    const {login, avatar_url} = eachItem
+    let div = document.createElement("div")
+    div.classList.add("user-div")
+    let img = document.createElement("img")
+    img.src = avatar_url 
+    img.classList.add("profile-image")
+    div.appendChild(img)
+    let heading = document.createElement("h1")
+    heading.classList.add("user-name")
+    heading.textContent=login
+    div.appendChild(heading)
+    let viewProfileButton = document.createElement("button")
+    viewProfileButton.textContent="View Profile"
+    viewProfileButton.classList.add("view-button")
+    div.appendChild(viewProfileButton)
+    root.appendChild(div)
+}
+
+async function getData(){
+    const userUrl = `https://api.github.com/search/users?q=${usernameValue}`
+    const repoUrl = `https://api.github.com/users/${usernameValue}/repos?per_page=100&sort=asc&client_id=${this.client_id}&client_secret=${client_secret}`
     const options = {
         method: 'GET'
     }
-    fetch(url,options)
+    const profile = await fetch(userUrl,options)
     .then((respone) => {
         return respone.json()
     })
     .then((jsonData) => {
         console.log(jsonData)
+        const {items} = jsonData
+        for (let eachItem of items){
+            displayUserProfile(eachItem)
+        }
+    })
+
+    const repo = await fetch(repoUrl,options)
+    .then((respone) => {
+        return respone.json()
+    })
+    .then((jsonData) => {
     })
 }
